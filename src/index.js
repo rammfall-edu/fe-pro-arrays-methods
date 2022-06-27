efed3d669c74530e960af36ab5e4d58192bd7f03
oleg-boyko <97238155+oleg-boyko@users.noreply.github.com>
2022-06-27 22:12:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { goods } from './goods.js';
  * @returns {*}
  */
 const brandFilter = (brand) => {
-return goods.filter((currentBrand) => currentBrand.brand.includes('brand'));
+return goods.filter((goodBrand) => goodBrand.brand.includes('Samsung'));
 };
 
 /**
