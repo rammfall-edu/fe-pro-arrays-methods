d1e8746fef57dad501c9eb96ae3b2581f78b42fd
oleg-boyko <97238155+oleg-boyko@users.noreply.github.com>
2022-06-29 18:02:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,44 +5,56 @@ import { goods } from './goods.js';
  * @returns {*}
  */
 const brandFilter = (brand) => {
-return goods.filter((goodBrand) => goodBrand[brand]);
+return goods.filter((goodBrand) => goodBrand.brand === brand);   
 };
 
 /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => {};
+const colorFilter = (color) => {
+return goods.filter((goodColor) => goodColor.color === color); 
+};
 
 /**
  * @param {string} model
  * @returns {*}
  */
-const modelFilter = (model) => {};
+const modelFilter = (model) => {
+return goods.filter((goodModel) => goodModel.model === model); 
+};
 
 /**
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => {};
+const memoryFilter = (memory) => {
+return goods.filter((goodMemory) => goodMemory.memory === memory); 
+};
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) => {};
+const priceFilter = (price) => {
+return goods.filter((goodPrice) => goodPrice.price === price); 
+};
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => {};
+const countryFilter = (country) => {
+return goods.filter((goodCountry) => goodCountry.country === country); 
+};
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => {};
+const osFilter = (os) => {
+return goods.filter((goodOs) => goodOs.os === os); 
+};
 
 /**
  * @param {number} from
@@ -53,7 +65,6 @@ return goods.filter((good) => (good.price >= from && good.price <= to));
 };
 
 const minPriceReducer = () => {};
-
 const maxPriceReducer = () => {};
 
 const toMaxSorter = () => {};
